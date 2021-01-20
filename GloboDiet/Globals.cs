b47f8562fca618c395d369e05bbb5398a3c73cc7
Stefan Meisegeier <s.meisegeier@rki.de>
2021-01-20 13:56:06 +0100

    attempts to write into footer

---
+++ GloboDiet/Globals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboDiet
+{
+    public enum ProcessMilestone
+    {
+        _0_NOTSTARTED,
+        _1_INTERVIEW,
+        _2_RESPONDENT,
+        _3_MEALS
+    }
+
+    public static class Globals
+    {
+        public static ProcessMilestone ProcessMilestone { get; set; }
+
+    }
+}
