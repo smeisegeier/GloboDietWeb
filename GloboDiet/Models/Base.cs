ab6fec66a0769edb5f8cebd6451d7f7ec2cf12d9
Stefan Meisegeier <s.meisegeier@rki.de>
2021-01-25 23:26:17 +0100

    prepared a model base

--- GloboDiet/Models/Base.cs
+++ GloboDiet/Models/Base.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 
 namespace GloboDiet.Models
 {
-    // This is just a test
-    public class Base
+    // This is just a test (EF, repo)
+    public abstract class Base
     {
-        public Base()
-        {
-
-        }
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; } 
+        public Guid Guid { get; set; }
     }
 }
