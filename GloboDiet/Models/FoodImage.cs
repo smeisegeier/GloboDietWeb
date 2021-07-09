0572bfedd0090362b36fbc15f06dfb89bc758fc3
Stefan Meisegeier <s.meisegeier@rki.de>
2021-07-09 10:21:48 +0200

    FoodImage started

---
+++ GloboDiet/Models/FoodImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboDiet.Models
+{
+    public class FoodImage : _ModelBase
+    {
+        public Uri IconPath { get; set; }
+
+        public List<FoodImage> GetSeedsFromMockup() => new List<FoodImage>()
+        {
+            new FoodImage() {Id=1, IconPath = new Uri("/images/apple1.png", UriKind.Relative)},
+            new FoodImage() {Id=2, IconPath = new Uri("/images/apple2.png", UriKind.Relative)},
+            new FoodImage() {Id=3, IconPath = new Uri("/images/fhir.png", UriKind.Relative)}
+        };
+    }
+}
